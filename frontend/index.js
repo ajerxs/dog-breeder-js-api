22dc24a11ac3928ff523828e8fa3eaaffdaffec8
Andrew Eames <ajerxs@gmail.com>
2022-01-08 20:44:53 -0500

    Test code added to js file for About link

--- frontend/index.js
+++ frontend/index.js
@@ -1,22 +1,27 @@
 const test  = document.getElementById("test");
 
 document.addEventListener("DOMContentLoaded", () => {
-    // testText();
     fetchTest();
 });
 
-// function testText() {
-//     let test = document.getElementById("test");
-//     test.innerHTML = "There should be stuff here.";
+// function fetchTest() {
+//     fetch('http://127.0.0.1:3000/couples')
+//     .then(resp => resp.json())
+//     .then(json => makeDiv(json));
 // };
 
-function fetchTest() {
-    fetch('http://127.0.0.1:3000/couples')
-    .then(resp => resp.json())
-    .then(json => makeDiv(json));
-};
+// function makeDiv(json) {
+//     let test = document.getElementById("test");
+//     test.innerText = json["data"][0]["attributes"]["father"];
+// };
 
-function makeDiv(json) {
-    let test = document.getElementById("test");
-    test.innerText = json["data"][0]["attributes"]["father"];
-};
+// ('about').observe('click', function(event) {
+//     Event.stop(event)
+// })
+
+document.getElementById('about').addEventListener('click', function() {
+    let container = document.getElementById('container');
+    let p = document.createElement("p");
+    p.innerHTML = 'If you can see this, that means the "About" link is working!'
+    container.appendChild(p);
+});
