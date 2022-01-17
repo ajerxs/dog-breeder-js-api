196eb2b3abf6aa67c3f0537715257dbcb47fa827
Andrew Eames <ajerxs@gmail.com>
2022-01-16 19:23:14 -0500

    Adjusted couples link to use created couple objects

--- frontend/couple.js
+++ frontend/couple.js
@@ -1,40 +1,4 @@
-document.getElementById('couples').addEventListener('click', function() {
-    clearPage();
-    fetchCouples();
-});
-
-function fetchCouples() {
-    fetch('http://127.0.0.1:3000/couples')
-    .then(resp => resp.json())
-    .then(json => couplesDivs(json))
-};
-
-function couplesDivs(json) {
-    for (let i = 0; i < json["data"].length; i++) {
-        let container = document.getElementById('container');
-
-        let couple = document.createElement("div");
-        couple.classList.add("couple");
-
-        let father = document.createElement("p");
-        father.classList.add("couple-contents");
-        father.innerHTML = "<strong>Father: </strong>" + json["data"][i]["attributes"]["father"];
-
-        let mother = document.createElement("p");
-        mother.classList.add("couple-contents");
-        mother.innerHTML = "<strong>Mother: </strong>" + json["data"][i]["attributes"]["mother"];
-
-        let breed = document.createElement("p");
-        breed.classList.add("couple-contents");
-        breed.innerHTML = "<strong>Breed: </strong>" + json["data"][i]["attributes"]["breed"];
-
-        couple.append(father, mother, breed);
-
-        container.appendChild(couple)
-    };
-};
-
-
+let couples = []
 
 class Couple {
     constructor(father, mother, breed) {
@@ -43,3 +7,51 @@ class Couple {
         this.breed = breed;
     }
 }
+
+
+document.getElementById('couples').addEventListener('click', function() {
+    clearPage();
+    fetchCouples();
+    couplesDivs();
+});
+
+function makeCouples(json) {
+    let data = json["data"];
+    for (let i = 0; i < data.length; i++) {
+        if (!(couples.find(e => e.father === data[i]["attributes"]["father"]) === couples.find(e => e.mother === data[i]["attributes"]["mother"]) === couples.find(e => e.breed == data[i]["attributes"]["breed"]))) {
+            let obj = new Couple(data[i]["attributes"]["father"], data[i]["attributes"]["mother"], data[i]["attributes"]["breed"]);
+            couples.push(obj);
+        };
+    };
+}
+
+function fetchCouples() {
+    fetch('http://127.0.0.1:3000/couples')
+    .then(resp => resp.json())
+    .then(json => makeCouples(json))
+};
+
+function couplesDivs(json) {
+    for (let i = 0; i < couples.length; i++) {
+        let container = document.getElementById('container');
+
+        let couple = document.createElement("div");
+        couple.classList.add("couple");
+
+        let father = document.createElement("p");
+        father.classList.add("couple-contents");
+        father.innerHTML = "<strong>Father: </strong>" + couples[i].father;
+
+        let mother = document.createElement("p");
+        mother.classList.add("couple-contents");
+        mother.innerHTML = "<strong>Mother: </strong>" + couples[i].mother;
+
+        let breed = document.createElement("p");
+        breed.classList.add("couple-contents");
+        breed.innerHTML = "<strong>Breed: </strong>" + couples[i].breed;
+
+        couple.append(father, mother, breed);
+
+        container.appendChild(couple)
+    };
+};
